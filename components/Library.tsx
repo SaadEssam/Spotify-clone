72d61a5e543bce6067cbb2d621516c0286946b27
SaadEssam <SaadEssam059@gmail.com>
2023-07-10 19:35:49 +0300

    Implement player functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,12 +3,13 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import { Song } from "@/types";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
-import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
-import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
 interface LibraryProps {
   songs: Song[];
@@ -18,6 +19,9 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
+
   const onClick = () => {
     if (!user) {
       return authModal.onOpen();
@@ -38,9 +42,15 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">{songs.map((item) => (
-        <MediaItem onClick={() => {}} key={item.id} data={item} />
-      ))}</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
